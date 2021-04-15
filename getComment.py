bc37055733377f44c31f7e3e4f7cc79005f0d364
Anihilium <72819587+Anihilium@users.noreply.github.com>
2021-04-15 09:41:29 +0200

    Delete credentials

--- getComment.py
+++ getComment.py
@@ -6,7 +6,7 @@ Created on Mon Apr 12 16:13:07 2021
 """
 
 import requests,re
-dataName=requests.get("https://Anihilium:Lolopk56@api.github.com/search/repositories?q=mongodb&per_page=5").json()
+dataName=requests.get("https://api.github.com/search/repositories?q=mongodb&per_page=5").json()
 reposName=[]
 for name in dataName["items"]:
     reposName.append(name["full_name"])
@@ -15,7 +15,7 @@ file = open("data.txt","w+",encoding="utf-8")
 for curRepo in reposName:
     count=1
     while True:
-        data=requests.get("https://Anihilium:Lolopk56@api.github.com/repos/"+curRepo+
+        data=requests.get("https://api.github.com/repos/"+curRepo+
                           "/comments?per_page=100&page="+str(count)).json()
         if len(data) == 0:
             break
