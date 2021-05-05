25d66f13aea4cb55b7d5f6650a9fc1d055a9386a
Anihilium <kristian.goupil@etudiant.univ-rennes1.fr>
2021-05-05 16:14:13 +0200

    Adding a gitignore file

--- getComment.py
+++ getComment.py
@@ -4,9 +4,9 @@ Created on Mon Apr 12 16:13:07 2021
 
 @author: lolop
 """
-
+"""
 import requests,re
-dataName=requests.get("https://Anihilium:Lolopk56@api.github.com/search/repositories?q=mongodb&per_page=5").json()
+dataName=requests.get("https://api.github.com/search/repositories?q=mongodb&per_page=5").json()
 reposName=[]
 for name in dataName["items"]:
     reposName.append(name["full_name"])
@@ -15,11 +15,33 @@ file = open("data.txt","w+",encoding="utf-8")
 for curRepo in reposName:
     count=1
     while True:
-        data=requests.get("https://Anihilium:Lolopk56@api.github.com/repos/"+curRepo+
+        data=requests.get("https://api.github.com/repos/"+curRepo+
                           "/comments?per_page=100&page="+str(count)).json()
         if len(data) == 0:
             break
         for comment in data:
             file.write(re.sub("(\n|\r|(\s(\s)+))","",comment["body"])+"\n")
         count+=1
+file.close()
+"""
+import requests,re
+dataName=requests.get("https://api.github.com/search/repositories?q=mongodb&per_page=1").json()
+reposName=[]
+for name in dataName["items"]:
+    reposName.append(name["full_name"])
+
+file = open("dataSubset.txt","w+",encoding="utf-8")
+for curRepo in reposName:
+    data=requests.get("https://api.github.com/repos/"+curRepo+
+                      "/comments?per_page=10&page=1").json()
+    for comment in data:
+        for key in comment:
+            if key=="user":
+                for userKey in comment[key]:
+                    file.write(str(comment[key][userKey])+';')
+            elif key=="body":
+                file.write(re.sub("(\n|\r|(\s(\s)+))","",comment["body"]))
+            else:
+                file.write(str(comment[key])+';')
+        file.write("\n")
 file.close()
